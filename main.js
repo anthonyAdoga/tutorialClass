418e9b9698ada7d55da5a9f0677c06945a18a4fe
anthonyAdoga <anthonyadoga9@gamil.com>
2023-08-03 13:53:54 +0100

    added uplink function

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
-const myName= document.getElementById("myName");
+const myName= document.getElementById("name");
+const mySchool= document.getElementById("school");
+
 
 myName.onclick=function(){
     myName.innerHTML="NVIT"
+}
+
+mySchool.onclick=function(){
+    mySchool.innerHTML="I study python"
 }
